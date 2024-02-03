105a6193434021db9f05fa15eb9ff81288d7671b
broly05 <akashinrealtime@gmail.com>
2024-02-03 18:55:56 +0530

    added a files

---
+++ diff-http1.1&http2.js
@@ -0,0 +1,17 @@
+// HTTP - HyperText Transfer Protocol , It is a top level Protocol for the world wide web (www).between
+// an end user and the server using computer network through the internet.
+// HTTP 1.1:
+//  Developed by Timothy Berners-Lee in 1989. Its usage is same as the http protocol but it is the 
+// new version from that. In this case, a client sends a text fromat request by calling the method 
+// GET or POST.server sends a response resource like an HTML page back to the client.
+// in addition to any images, stylesheets, or other resources called for in the HTML. 
+// all of the resources are returned to the client in the first call for data. 
+
+// HTTP 2:
+// HTTP/2 began as the SPDY protocol, developed primarily at Google for reduce web page load latency 
+// by using techniques such as compression, multiplexing, and prioritization.All the core concepts found in
+//  HTTP 1.1, such as HTTP methods, status codes, URIs, and header fields, remain in place. Instead, 
+//  HTTP/2 modifies how the data is framed and transported between the client and server, both 
+//  of which manage the entire process, and hides application complexity within the new framing layer. 
+//  As a result, all existing applications can be delivered without modification.
+//  as my knowledge these are the thinks are important.
