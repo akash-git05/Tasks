e2cada9fd9fb5e3787679af55ff8fb50e84d1c43
broly05 <akashinrealtime@gmail.com>
2024-02-03 19:06:40 +0530

    added files

---
+++ day1/objblog.js
@@ -0,0 +1,10 @@
+// let obj={
+//     name:"sam",
+//      age:"25"
+//        };
+//        console.log(obj.name,obj.age);
+// consider this code to create and run the object
+// it will return the corresponding output as
+// output :sam 25   
+//  object is not having an index so we can't able to call it using index value
+//  so we have to use the key name and dot operator to access the object
