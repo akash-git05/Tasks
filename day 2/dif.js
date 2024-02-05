d26ee84a490b19302aca0e946f72ded72dfb54c7
broly05 <akashinrealtime@gmail.com>
2024-02-05 15:24:54 +0530

    added a file

---
+++ day 2/dif.js
@@ -0,0 +1,20 @@
+// write a blog on difference between document and window object
+//window:
+// window is an object that as  many predefined properties
+// window is called the big object than ever 
+// window object is that we're seeing a webpage I mean a full page 
+// some of the exampales of the window object:
+
+// alert()	Displays an alert box with a message and an OK button
+// atob()	Decodes a base-64 encoded string
+// blur()	Removes focus from the current window
+
+
+// Document:
+// document is one of the method inside the window Object.
+// It also having many methods , but compared to window it has less .
+// some of the examples of the document methods:
+
+// document.write(text)	Write into the HTML output stream
+// document.getElementById(id)	Find an element by element id
+// document.getElementsByClassName(name)	Find elements by class name
